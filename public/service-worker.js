3ea49adc72cbc8a07f26155dc48218a39836f34e
unknown <lovinmathew@hotmail.com>
2020-05-05 18:03:44 -0500

    Changed

---
+++ public/service-worker.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'cache-v1';
+
+var urlsToCache = [
+    "/",
+    "/styles.css",
+    "/index.js",
+    "/index.html",
+    "/db.js",
+    "/assets/icons/favicon-192x192.png",
+    "/assets/icons/favicon-512x512.png"
+];
+
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request).then(function(response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/index.html");
+                }
+            });
+        })
+    );
+});
